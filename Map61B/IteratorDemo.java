1535f4bbbcb8bada55e917f40ca1c9d811edd189
jesushedd <jesushe99@gmail.com>
2024-08-26 12:09:00 -0500

    Iterable demo

---
+++ Map61B/IteratorDemo.java
@@ -0,0 +1,25 @@
+package Map61B;
+
+public class IteratorDemo {
+    public static void main(String[] args) {
+        ArrayMap<String, Integer> am = new ArrayMap<String, Integer>();
+
+        am.put("hello", 5);
+        am.put("syrups", 10);
+        am.put("kingdom", 10);
+
+        /*ArrayMap<String, Integer>.KeyIterator ami = am.new KeyIterator();
+
+        while (ami.hasNext()){
+            System.out.println(ami.next());
+        }*/
+
+        for (String s : am){
+            System.out.println(s);
+        }
+
+
+
+
+    }
+}
